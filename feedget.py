bbf0e2e0e7be405f7d1953c5e71275bfc583f628
Someya Naoki <someya.naoki@jp.fujitsu.com>
2016-07-11 13:35:44 +0900

    FIX: 1) Setup from config file. 2) Sanitizing sammary.

--- feedget.py
+++ feedget.py
@@ -5,29 +5,56 @@ import feedparser
 import time
 # from datetime import datetime
 import pandas as pd
-from mattermost_client import mattermost_cl as mmc
+from mattermost_client import mattermost_cl
+import logging
+import re
+from ConfigParser import SafeConfigParser
+import sys
 
-MATTERMOSTURL = "http://10.125.75.11:8065/hooks/qs4uzy4b1py98qjpiut93sewzw"
-RSS_URL = "http://b.hatena.ne.jp/hotentry/it.rss"
-MATTERMOST_USER = 'はてブTECHフィード'
+DEFAULT_COFIG_PATH = "./config.ini"
 
-mmc = mmc.MatterMostClient(MATTERMOSTURL)
+tag_re = re.compile(r'(<!--.*?-->|<[^>]*>)')
+logger = logging.getLogger(__name__)
 
-already_print_feeds = pd.Series([])
 
-print("Start get feed...")
+def main():
 
-while True:
-    time.sleep(600)
+    try:
+        param = sys.argv
+        config_file = param[1]
+    except IndexError:
+        print("Use default config file(./config.ini)")
+        print("If you specify original config file,")
+        print("Usage: feedget.py [config file path]")
+        config_file = DEFAULT_COFIG_PATH
+
+    config = SafeConfigParser()
+    config.read(config_file)
+
+    MATTERMOSTURL = config.get('mattermost', 'url')
+    RSS_URL = config.get('rss', 'url')
+    MATTERMOST_USER = config.get('mattermost', 'username')
+    GETFEED_INTERVAL = float(config.get('rss', 'interval'))
+    SENDMESSAGE_INTERVAL = float(config.get('mattermost', 'interval'))
+
+    mmc = mattermost_cl.MatterMostClient(MATTERMOSTURL)
+
+    # Get First RSS feed. TODO: Get id from database
+    print("Start get feed from %s" % (RSS_URL))
     feed = feedparser.parse(RSS_URL)
-    entries = pd.DataFrame(feed.entries)
-    new_entries = entries[~entries['id'].isin(already_print_feeds)]
+    initial_entries = pd.DataFrame(feed.entries)
+    already_print_feeds = pd.Series(initial_entries['id'])
+    while True:
+        time.sleep(GETFEED_INTERVAL)
+        feed = feedparser.parse(RSS_URL)
+        entries = pd.DataFrame(feed.entries)
+        new_entries = entries[~entries['id'].isin(already_print_feeds)]
+        if not new_entries.empty:
+            for key, row in new_entries.iterrows():
+                feedinfo = "[**%s**](%s)\n\n>%s" % (row['title'],  row['link'], tag_re.sub('', row['summary']))
+                mmc.send_message(feedinfo, username=MATTERMOST_USER)
+                time.sleep(SENDMESSAGE_INTERVAL)
+        already_print_feeds = already_print_feeds.append(new_entries['id'])
 
-    if not new_entries.empty:
-        for key, row in new_entries.iterrows():
-            feedinfo = "[**%s**](%s)\n\n>%s" % (row['title'],  row['link'], row['summary'].replace("|", ":"))
-            mmc.send_message(feedinfo, username=MATTERMOST_USER)
-            time.sleep(10)
-
-    already_print_feeds = already_print_feeds.append(new_entries['id'])
-    break
+if __name__ == "__main__":
+    main()
